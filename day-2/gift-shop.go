c098652273df64cf336438fa720b075f01121b10
Eloi Bernard <eloi0bernard@gmail.com>
2025-12-04 11:39:28 +0100

    feat: day 2 - read and parse input

--- day-2/gift-shop.go
+++ day-2/gift-shop.go
@@ -2,7 +2,10 @@ package day2
 
 import (
 	_ "embed"
+	"fmt"
 	"log/slog"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
@@ -26,14 +29,38 @@ func SolveGiftShop() {
 }
 
 func formatInput(rawInput string) ([]string, error) {
-	return []string{}, nil
+	trimedRawInput := strings.TrimRight(rawInput, "\n")
+	if len(trimedRawInput) == 0 {
+		return nil, fmt.Errorf("empty input.txt file")
+	}
+	inputList := strings.Split(trimedRawInput, ",")
+	return inputList, nil
 }
 
 func inputsToRanges(input []string) ([]Range, error) {
 	result := []Range{}
+	for _, entry := range input {
+		rangeEntry, err := parseRange(entry)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, rangeEntry)
+	}
 	return result, nil
 }
 
 func parseRange(entry string) (Range, error) {
-	return Range{}, nil
+	values := strings.Split(entry, "-")
+	if len(values) != 2 {
+		return Range{}, fmt.Errorf("invalid range %s", entry)
+	}
+	min, err := strconv.Atoi(values[0])
+	if err != nil {
+		return Range{}, err
+	}
+	max, err := strconv.Atoi(values[1])
+	if err != nil {
+		return Range{}, err
+	}
+	return Range{min: min, max: max}, nil
 }
